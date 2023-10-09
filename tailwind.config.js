d12ad16df7a414492950b4bce9616be4383c99ff
Michael Dirnberger <michael.dirnberger@attribui.com>
2023-10-09 20:26:08 +0200

    Improved order of plugins.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui'), require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
     themes: ["light"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "dracula", // name of one of the included themes for dark mode
