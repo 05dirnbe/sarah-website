d3628e725b70cc788af3e55032dbb50524486c32
Michael Dirnberger <michael.dirnberger@attribui.com>
2023-10-12 16:17:47 +0200

    Implemented syntax highlighting for dark and light mode.

--- .eleventy.js
+++ .eleventy.js
@@ -154,23 +154,23 @@ module.exports = eleventyConfig => {
   //     );
   // });
 
-  // //Setup for html minifier
-  // eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
-  //   if (
-  //     process.env.ELEVENTY_PRODUCTION &&
-  //     outputPath &&
-  //     outputPath.endsWith('.html')
-  //   ) {
-  //     let minified = htmlmin.minify(content, {
-  //       useShortDoctype: true,
-  //       removeComments: true,
-  //       collapseWhitespace: true,
-  //     })
-  //     return minified
-  //   }
-  //
-  //     return content
-  // });
+  //Setup for html minifier
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    if (
+      process.env.ELEVENTY_PRODUCTION &&
+      outputPath &&
+      outputPath.endsWith('.html')
+    ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      })
+      return minified
+    }
+
+      return content
+  });
 
 
   return {
